c28baeca3e7788fed83c2c2fca643a1409d084ed
Aishwarya <76867908+aishwaryabit1603@users.noreply.github.com>
2023-03-02 20:37:33 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bottom</title>
+    <style>
+        .footer_left{
+            position: fixed;
+            padding: 10px 10px 0px 10px;
+            bottom: 10%;
+            width: 100%;
+            height: 40px;
+            background: #;
+        }
+        .footer_right{
+            
+        }
+    </style>
+</head>
+<body>
+    <div class= "footer_left">
+            BLOOD DONATION WEBSITE PROJECT :<br>
+            MADE BY AISHWARYA<br>
+            CLASS : BCA 6th SEM Section : A
+            ROLL NO : 2241602<br>
+    </div>
+    <div class = "footer_right">
+            <a href="mailto:aishwaryasingla123@gmail.com">CONTACT US</a>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="TEXT/CSS" href="style.css"/>
+    <title>BLOOD DONATION</title>
+</head>
+<body>
+    <?php
+        //require ('header.php');
+        require ('footer.php'); 
+    ?>
+    <div class = "vertical_menu">
+        <a href = "#" class = "active">HOME</a>
+        <a href = "admin.php">ADMIN LOGIN</a>
+        <a href = "#">BE A DONOR</a>
+        <a href = "#">FIND A DONOR</a>
+        <a href = "#">LOGIN</a>
+    </div>
+
+</body>
+</html>
