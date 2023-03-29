5d138c10f1b3b02c2b3727d602ee0309dced33a7
Aishwarya <aishwaryasingla123@gmail.com>
2023-03-29 21:51:33 +0530

    yes

--- register.php
+++ register.php
@@ -40,7 +40,7 @@
         echo '<script language = "javascript">';
         echo 'alert("Message Successfully Sent !!")';
         echo '</script>';
-        if (isset($_SESSION['sess_user_id']) && !empty($_SESSION['sess_user_id']))
+        if (isset($_SESSION['session_user_id']) && !empty($_SESSION['session_user_id']))
         {
           header("location: logout.php");
         }
@@ -157,169 +157,169 @@
           form.classList.add('form--no') 
         });
         function dobcheck()
-{
-    var birth = document.getElementById('bday')
-    if(birth != "")
-    {
-
-        var record=document.getElementById('bday').value.trim();
-        var currentdate=new Date();    
-        var day1 = currentdate3.getDate();   
-        var month1 = currentdate3.getMonth();
-        month1++;     
-        var year11 = currentdate3.getFullYear()-17;
-        var year2= currentdate3.getFullYear()-100;   
-        var record_day1=record.split("/");
-        var sum=record_day1[1]+'/'+record_day1[0]+'/'+record_day1[2];  
-        var current= month1+'/'+day1+'/'+year11;
-        var current1= month1+'/'+day1+'/'+year2;
-        var d1=new Date(current)
-        var d2=new Date(current1)
-        var record1 = new Date(sum);      
-        if(record1 > d1)
         {
-          alert("Sorry ! Minors need parential guidance to use this website");
-          document.getElementById('bday').blur();
-          document.getElementById('bday').value="";
-          document.getElementById('bday').focus();
-          return false;
-        }
-    } 
-}
-      </script>
-      <style>
-        $font-family:   "Roboto";
-$font-size:     14px;
-$color-primary: #ABA194;
+          var birth = document.getElementById('bday')
+          if(birth != "")
+          {
 
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+              var record=document.getElementById('bday').value.trim();
+              var currentdate=new Date();    
+              var day1 = currentdate3.getDate();   
+              var month1 = currentdate3.getMonth();
+              month1++;     
+              var year11 = currentdate3.getFullYear()-17;
+              var year2= currentdate3.getFullYear()-100;   
+              var record_day1=record.split("/");
+              var sum=record_day1[1]+'/'+record_day1[0]+'/'+record_day1[2];  
+              var current= month1+'/'+day1+'/'+year11;
+              var current1= month1+'/'+day1+'/'+year2;
+              var d1=new Date(current)
+              var d2=new Date(current1)
+              var record1 = new Date(sum);      
+              if(record1 > d1)
+              {
+                alert("Sorry ! Minors need parential guidance to use this website");
+                document.getElementById('bday').blur();
+                document.getElementById('bday').value="";
+                document.getElementById('bday').focus();
+                return false;
+              }
+          } 
+      }
+    </script>
+    <style>
+      $font-family:   "Roboto";
+      $font-size:     14px;
+      $color-primary: #ABA194;
 
-body {
-    font-family: $font-family;
-    font-size: $font-size;
-    color = $color-primary;
-    background-size: 200% 100% !important;
-    animation: move 10s ease infinite;
-    transform: translate3d(0, 0, 0);
-    background: linear-gradient(45deg, #49D49D 10%, #A2C7E5 90%);
-    height: 100vh
-}
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
 
-.user {
-    width: 90%;
-    max-width: 340px;
-    margin: 10vh auto;
-}
+      body {
+        font-family: $font-family;
+        font-size: $font-size;
+        color = $color-primary;
+        background-size: 200% 100% !important;
+        animation: move 10s ease infinite;
+        transform: translate3d(0, 0, 0);
+        background: linear-gradient(45deg, #49D49D 10%, #A2C7E5 90%);
+        height: 100vh
+      }
 
-.user__header {
-    text-align: center;
-    opacity: 0;
-    transform: translate3d(0, 500px, 0);
-    animation: arrive 500ms ease-in-out 0.7s forwards;
-}
+      .user {
+        width: 90%;
+        max-width: 340px;
+        margin: 10vh auto;
+      }
 
-.user__title {
-    font-size: $font-size;
-    margin-bottom: -10px;
-    font-weight: 500;
-    color: white;
-}
-
-.form {
-    margin-top: 40px;
-    border-radius: 6px;
-    overflow: hidden;
-    opacity: 0;
-    transform: translate3d(0, 500px, 0);
-    animation: arrive 500ms ease-in-out 0.9s forwards;
-}
-
-.form--no {
-    animation: NO 1s ease-in-out;
-    opacity: 1;
-    transform: translate3d(0, 0, 0);
-}
-
-.form__input {
-    display: block;
-    width: 100%;
-    padding: 20px;
-    font-family: $font-family;
-    -webkit-appearance: none;
-    border: 0;
-    outline: 0;
-    transition: 0.3s;
-    
-    &:focus {
-        background: darken(#fff, 3%);
-    }
-}
-
-.btn {
-    display: block;
-    width: 100%;
-    padding: 20px;
-    font-family: $font-family;
-    -webkit-appearance: none;
-    outline: 0;
-    border: 0;
-    color: black;
-    text-decoration-thickness: 5px; 
-    background: $color-primary;
-    transition: 0.3s;
-    
-    &:hover {
-        background: darken($color-primary, 5%);
-    }
-}
-
-@keyframes NO {
-  from, to {
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-  }
-
-  10%, 30%, 50%, 70%, 90% {
-    -webkit-transform: translate3d(-10px, 0, 0);
-    transform: translate3d(-10px, 0, 0);
-  }
-
-  20%, 40%, 60%, 80% {
-    -webkit-transform: translate3d(10px, 0, 0);
-    transform: translate3d(10px, 0, 0);
-  }
-}
-
-@keyframes arrive {
-    0% {
+      .user__header {
+        text-align: center;
         opacity: 0;
-        transform: translate3d(0, 50px, 0);
-    }
-    
-    100% {
+        transform: translate3d(0, 500px, 0);
+        animation: arrive 500ms ease-in-out 0.7s forwards;
+      }
+
+      .user__title {
+        font-size: $font-size;
+        margin-bottom: -10px;
+        font-weight: 500;
+        color: white;
+      }
+
+      .form {
+        margin-top: 40px;
+        border-radius: 6px;
+        overflow: hidden;
+        opacity: 0;
+        transform: translate3d(0, 500px, 0);
+        animation: arrive 500ms ease-in-out 0.9s forwards;
+      }
+
+      .form--no {
+        animation: NO 1s ease-in-out;
         opacity: 1;
         transform: translate3d(0, 0, 0);
-    }
-}
+      }
 
-@keyframes move {
-    0% {
-        background-position: 0 0
-    }
+      .form__input {
+        display: block;
+        width: 100%;
+        padding: 20px;
+        font-family: $font-family;
+        -webkit-appearance: none;
+        border: 0;
+        outline: 0;
+        transition: 0.3s;
+        
+        &:focus {
+            background: darken(#fff, 3%);
+        }
+      }
 
-    50% {
-        background-position: 100% 0
-    }
+      .btn {
+        display: block;
+        width: 100%;
+        padding: 20px;
+        font-family: $font-family;
+        -webkit-appearance: none;
+        outline: 0;
+        border: 0;
+        color: black;
+        text-decoration-thickness: 5px; 
+        background: $color-primary;
+        transition: 0.3s;
+        
+        &:hover {
+            background: darken($color-primary, 5%);
+        }
+      }
 
-    100% {
-        background-position: 0 0
-    }
-}
-      </style>
+      @keyframes NO {
+      from, to {
+        -webkit-transform: translate3d(0, 0, 0);
+        transform: translate3d(0, 0, 0);
+      }
+
+      10%, 30%, 50%, 70%, 90% {
+        -webkit-transform: translate3d(-10px, 0, 0);
+        transform: translate3d(-10px, 0, 0);
+      }
+
+      20%, 40%, 60%, 80% {
+        -webkit-transform: translate3d(10px, 0, 0);
+        transform: translate3d(10px, 0, 0);
+      }
+      }
+
+      @keyframes arrive {
+        0% {
+            opacity: 0;
+            transform: translate3d(0, 50px, 0);
+        }
+        
+        100% {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+        }
+      }
+
+      @keyframes move {
+        0% {
+            background-position: 0 0
+        }
+
+        50% {
+            background-position: 100% 0
+        }
+
+        100% {
+            background-position: 0 0
+        }
+      }
+    </style>
 </head>
 <body>
 <div class="user">
@@ -328,6 +328,7 @@ body {
     </header>
   
       <form class="form">
+        <?php include('errors.php'); ?>
         <div class="form__group">
           <input type="text" placeholder="Username" name = "user_name" class="form__input" required />
         </div>
