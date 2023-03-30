9ff547dcef5210c9248935481fa28eddd2315b02
Aishwarya <aishwaryasingla123@gmail.com>
2023-03-30 17:52:40 +0530

    m

--- donor.sql
+++ donor.sql
@@ -11,7 +11,7 @@ CREATE TABLE donors(
     sex varchar(20) NOT NULL,
     bloodgroup varchar(20) NOT NULL,
     mobile_no varchar(20) NOT NULL UNIQUE,
-    email varchar(20) PRIMARY KEY,
+    email varchar(20) NOT NULL UNIQUE,
     state varchar(20) NOT NULL,
     district varchar(20) NOT NULL
 )  ENGINE = InnoDB DEFAULT CHARSET = latin1 COLLATE = latin1_swedish_ci;
