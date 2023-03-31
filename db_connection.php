cd94484693a15de3740af13245ef6d8d239fa3f0
Aishwarya <aishwaryasingla123@gmail.com>
2023-03-31 19:03:08 +0530

    m

--- db_connection.php
+++ db_connection.php
@@ -6,7 +6,7 @@
 
     // connecting to database
     $connection = mysqli_connect($db_host,$db_user,$db_password,$db_database);
-    // if connection fails alert msgbox
+    // if connection fails alert message box
     if(!$connection)
     {
         die('Connect not Connect: '.mysqli_error($connection));
--- register.php
+++ register.php
@@ -1,31 +1,74 @@
 <?php
   ob_start();
   require 'db_connection.php';
-  
   $error = [];
-  if(!empty($_POST['submit'] ))
+  if(!empty($_POST['submit']))
   {
-    if(!empty($_POST(['name'])) && !empty($_POST(['dob'])) && !empty($_POST(['sex'])) && !empty($_POST(['blood_type'])) && !empty($_POST(['mobile_no'])) && !empty($_POST(['email'])) &&
-    !empty($_POST(['state_input'])) && !empty($_POST(['district_input'])) && !empty($_POST(['password_1'])) &&
-      !empty($_POST(['password_2'])))
-      {
-        $f_name = mysqli_real_escape_string($connection, $_POST['name']);
-        $birthday = mysqli_real_escape_string($connection, $_POST['dob']);
-        $sex = mysqli_real_escape_string($connection, $_POST['sex']);
-        $blood = mysqli_real_escape_string($connection, $_POST['blood_type']);
-        $mobile = mysqli_real_escape_string($connection, $_POST['mobile_no']);
-        $email = mysqli_real_escape_string($connection, $_POST['email']);
-        $state = mysqli_real_escape_string($connection, $_POST['state_input']);
-        $district = mysqli_real_escape_string($connection, $_POST['district_input']);
-        $pw_1  = mysqli_real_escape_string($connection, md5($_POST['password_1']));
-        $pw_2  = mysqli_real_escape_string($connection, md5($_POST['password_2']));
+    if(!empty($_POST(['name'])) && !empty($_POST(['dob'])) && !empty($_POST(['sex'])) && !empty($_POST(['blood_type'])) && !empty($_POST(['mobile_no'])) && !empty($_POST(['email'])) &&!empty($_POST(['state_input'])) && !empty($_POST(['district_input'])) && !empty($_POST(['password_1'])) &&!empty($_POST(['password_2'])))
+    {
+      $f_name = mysqli_real_escape_string($connection, $_POST['name']);
+      $birthday = mysqli_real_escape_string($connection, $_POST['dob']);
+      $sex = mysqli_real_escape_string($connection, $_POST['sex']);
+      $blood = mysqli_real_escape_string($connection, $_POST['blood_type']);
+      $mobile = mysqli_real_escape_string($connection, $_POST['mobile_no']);
+      $email = mysqli_real_escape_string($connection, $_POST['email']);
+      $state = mysqli_real_escape_string($connection, $_POST['state_input']);
+      $district = mysqli_real_escape_string($connection, $_POST['district_input']);
+      $pw_1  = mysqli_real_escape_string($connection, md5($_POST['password_1']));
+      $pw_2  = mysqli_real_escape_string($connection, md5($_POST['password_2']));
 
-        
+      if(strlen($mobile < 10))
+      {
+        array_push($error,"Mobile no should be = 10"); 
+      }
+      if($pw_1 != $pw_2)
+      {
+        array_push($error,"Confirm password does not match");
+      }
+
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+      {
+        array_push($error,"Not a valid email address");  
+      }
+
+      // validation donor > 18
+      $today = date("Y-m-d");
+      $diff = date_diff(date_create($birthday), date_create($today));
+      if($diff < 18)
+      {
+        array_push($error,"Not Eligible to donate blood");
+      }
+
+      if(count($error) == 0)
+      {
+        $sql = "INSERT INTO donors VALUES ('$pw_1','$f_name','$birthday','$sex','$blood','$mobile','$email','$state','$district')";
+        $result = mysqli_query($connection,$sql);
+        if($result)
+        {
+          echo '<script type ="text/JavaScript">';  
+          echo 'alert("Registration Successful ")';  
+          echo '</script>';
+        }
+        else
+        {
+          die(mysqli_error($connection));
+        }
       }
       else
       {
-        echo "Registration Failed";
+        foreach($error as $error)
+        {
+          echo '<script type ="text/JavaScript">';  
+          echo 'alert($error)';  
+          echo '</script>';
+        }
       }
+    }
   }
-  
+  else
+  {
+    echo "<script type ='text/JavaScript'>";  
+    echo "alert('Registration Failed')";  
+    echo '</script>';
+  }
 ?>
