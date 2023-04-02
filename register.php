13a27828b2ddb1cb1115eeacd0d819a80ef71c36
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-02 11:54:31 +0530

    ,

--- register.php
+++ register.php
@@ -2,38 +2,48 @@
   require 'db_connection.php';
   if(isset($_POST['submit']))
   {
+     echo "1";
     if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']) && !empty($_POST['password_1']) &&!empty($_POST['password_2']))
     {
+
+      echo "2";
+
       $f_name = $_POST['name'];
       $birthday = $_POST['dob'];
       $sex = $_POST['sex'];
       $blood = $_POST['blood_type'];
       $mobile = $_POST['mobile_no'];
-      $email_1 = $_POST['email'];
-      
-      $email = filter_var($email_1, FILTER_SANITIZE_EMAIL);
-      echo $email;
+      $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
       $state = $_POST['state_input'];
       $district = $_POST['district_input'];
       $pw_1  = $_POST['password_1'];
       $pw_2  = $_POST['password_2'];
 
+      echo "3";
       $query = "Select * from donors where email = '$email' || mobile_no = '$mobile' ";
       $res = mysqli_query($connection,$query);
+      echo $res;
+
       if($res)
       {
+        echo "4";
         $num = mysqli_num_rows($res);
         if($num > 0)
         {
-          header("location: register.html");
+          echo "5";
+          echo "USER ALREADY EXISTS";
+          header('location: register.html');
         }
       }
       else
       {
+        echo "6";
         $sql = "INSERT INTO donors (password,name,dob,sex,bloodgroup,mobile_no,email,state,district)VALUES ('$pw_1','$f_name','$birthday','$sex','$blood','$mobile','$email','$state','$district')";
         $result = mysqli_query($connection,$sql);
+
         if($result)
         {
+          echo "7";
           echo '<script>alert("Registration Successful !! ")</script>';
           header('Location:login.html');
         }
