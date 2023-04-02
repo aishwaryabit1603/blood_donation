2740612d9925b849df02bd4ff1f2a81175dfd2dd
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-02 12:20:48 +0530

    m

--- register.php
+++ register.php
@@ -2,12 +2,9 @@
   require 'db_connection.php';
   if(isset($_POST['submit']))
   {
-     echo "1";
-    if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']) && !empty($_POST['password_1']) &&!empty($_POST['password_2']))
-    {
-
-      echo "2";
-
+  if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']) && !empty($_POST['password_1']) &&!empty($_POST['password_2']))
+  {
+      $error = 0;
       $f_name = $_POST['name'];
       $birthday = $_POST['dob'];
       $sex = $_POST['sex'];
@@ -19,42 +16,61 @@
       $pw_1  = $_POST['password_1'];
       $pw_2  = $_POST['password_2'];
 
-      echo "3";
-      $query = "Select * from donors where email = '$email' || mobile_no = '$mobile' ";
-      $res = mysqli_query($connection,$query);
-
-      if($res)
+      if($pw_1 != $pw_2)
       {
-        echo "4";
-        $num = mysqli_num_rows($res);
-        echo count($num);
-        if($num > 0)
+        $error = $error + 1;
+      }
+
+      function validateAge($birthday, $age = 18)
+      {
+          // $birthday can be UNIX_TIMESTAMP or just a string-date.
+          if(is_string($birthday)) {
+              $birthday = strtotime($birthday);
+          }
+
+          // check
+          // 31536000 is the number of seconds in a 365 days year.
+          if(time() - $birthday < $age * 31536000)  {
+              return false;
+          }
+
+          return true;
+      }
+      $ans = validateAge($birthday,18);
+      if(ans == 0)
+      {
+        $error = $error + 1;
+      }
+
+      if($error == 0)
+      {
+        $query = "Select * from donors where email = '$email' || mobile_no = '$mobile' ";
+        $res = mysqli_query($connection,$query);
+        if($res)
         {
-          echo "5";
-          echo "USER ALREADY EXISTS";
-          header('location: register.html');
+          $num = mysqli_num_rows($res);
+          if($num > 0)
+          {
+            echo '<script>alert("USER ALREADY EXISTS")</script>';
+            header('location: register.html');
+          }
+          else
+          {
+            $sql = "INSERT INTO donors (password,name,dob,sex,bloodgroup,mobile_no,email,state,district)VALUES ('$pw_1','$f_name','$birthday','$sex','$blood','$mobile','$email','$state','$district')";
+            $result = mysqli_query($connection,$sql);
+            if($result)
+            {
+              echo '<script>alert("Registration Successful !! ")</script>';
+              header('Location:login.html');
+            }
+            else
+            {
+              die(mysqli_error($connection));
+            }
+          }
         }
       }
-      else
-      {
-        echo "6";
-        $sql = "INSERT INTO donors (password,name,dob,sex,bloodgroup,mobile_no,email,state,district)VALUES ('$pw_1','$f_name','$birthday','$sex','$blood','$mobile','$email','$state','$district')";
-        $result = mysqli_query($connection,$sql);
-
-        if($result)
-        {
-          echo "7";
-          echo '<script>alert("Registration Successful !! ")</script>';
-          header('Location:login.html');
-        }
-        else
-        {
-          echo 'hi';
-          die(mysqli_error($connection));
-        }
-      }
-    }
-  }
+     }
   else
   {
     echo "<script type ='text/JavaScript'>";  
@@ -62,6 +78,7 @@
     echo '</script>';
     header("Location: register.html");
   }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
