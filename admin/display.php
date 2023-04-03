6c11c3cb026288677af3eff80ffacc9a2b935dc4
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-03 23:49:17 +0530

    a

--- admin/display.php
+++ admin/display.php
@@ -133,18 +133,8 @@
                 <td><?php echo $row['state'] ?></td>
                 <td><?php echo $row['district'] ?></td>
                 <td>
-                <button class="Remove" id ="update" onclick="redirect1()">Update</button>
-                    <script>
-                        function redirect1() {
-                            window.location.href = "http://localhost:8080/blood_donation/admin/update.php" + '?id_no='+id_no;
-                        }
-                    </script>
-                <button class="Remove" id = "delete" onclick="redirect2()">Delete</button>
-                    <script>
-                    function redirect2() {
-                        window.location.href = "http://localhost:8080/blood_donation/admin/delete.php" + '?id_no='+ id_no;
-                    }
-                </script>
+                <button class="Remove" id ="update" ><a href = "C:\xampp\htdocs\blood_donation\admin\update.html">Update</a></button>
+                <button class="Remove" id = "delete" href = "C:\xampp\htdocs\blood_donation\admin\delete.html" ><a>Delete</a></button>
                 </td> 
             </tr>
             <?php
@@ -154,7 +144,8 @@
         <tbody>
             <tr>
                 <td colspan="4" ></td>
-                <td><button class="Add" id="Add"><a href = "C:\xampp\htdocs\blood_donation\add_user.php">Add</a></button>
+                <td><button class="Add" id="Add">
+                <a href="C:\xampp\htdocs\blood_donation\admin\add_user.html">ADD</a>
 
             </td>
             </tr>
