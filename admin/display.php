398f4441adf8f483f574dbaad5a9f84e0ea438e9
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-04 10:57:34 +0530

    A

--- admin/display.php
+++ admin/display.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Display USERS</title>
+    <title>DISPLAY USERS</title>
     <link rel="icon" type="image/x-icon" href="C:\xampp\htdocs\blood_donation\images\icon2.ico" />
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
