b88ac43025a91d5a0c8a7d281012ffc882e05f07
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-04 21:11:20 +0530

    m

--- admin/update.php
+++ admin/update.php
@@ -2,12 +2,11 @@
   try
   {
     require 'C:\xampp\htdocs\blood_donation\db_connection.php' ;
-    session_start();
     if(isset($_POST['update']))
     {
       if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']))
       {
-          
+          $ids = $_POST['id'];
           $f_name = strtolower($_POST['name']) ;
           $f_name = ucfirst($f_name);
           $birthday = $_POST['dob'];
@@ -21,13 +20,13 @@
           $res = "UPDATE donors 
           SET name = '$f_name',dob = '$birthday',sex = '$sex',bloodgroup = '$blood',
           mobile_no = '$mobile',email = '$email',state = '$state',district = '$district' 
-          WHERE id_no =  " . $_SESSION['row_id'] . ";";
+          WHERE id_no = '$ids'";
   
           $results = mysqli_query($connection,$res);
   
           if($results)
           {
-              echo "<script type ='text/JavaScript'>";  
+              echo '<script type = "text/JavaScript">';  
               echo "alert('Updated Successfully')";
               echo '</script>';
           }
