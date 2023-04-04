4552bc11ac0288147beca083934b3feb941da752
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-04 15:02:33 +0530

    ,

---
+++ admin/update.php
@@ -0,0 +1,79 @@
+<?php
+  require 'C:\xampp\htdocs\blood_donation\db_connection.php';
+  if(isset($_POST['update']))
+  {
+    if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']) && !empty($_POST['password_1']))
+    {
+        $error = 0;
+        $f_name = ucfirst($_POST['name']);
+        $birthday = $_POST['dob'];
+        $sex = $_POST['sex'];
+        $blood = $_POST['blood_type'];
+        $mobile = $_POST['mobile_no'];
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $state = $_POST['state_input'];
+        $district = $_POST['district_input'];
+        $pw_1  = $_POST['password_1'];
+
+        function validateAge($birthday, $age = 18)
+        {
+          // $birthday can be UNIX_TIMESTAMP or just a string-date.
+          if(is_string($birthday))
+          {
+            $birthday = strtotime($birthday);
+          }
+          // check
+          // 31536000 is the number of seconds in a 365 days year.
+          if(time() - $birthday < $age * 31536000)  {
+              return false;
+          }
+
+          return true;
+        }
+        $ans = validateAge($birthday,18);
+        if($ans == 0)
+        {
+          $error = $error + 1;
+        }
+
+        if($error == 0)
+        {
+          $query = "Select * from donors where email = '$email' || mobile_no = '$mobile' ";
+          $res = mysqli_query($connection,$query);
+          if($res)
+          {
+            $num = mysqli_num_rows($res);
+            if($num > 0)
+            { 
+              echo '<script>alert("USER ALREADY EXISTS")</script>';
+              header('location: C:\xampp\htdocs\blood_donation\admin\display.php');
+            }
+            else
+            {
+              $sql = "UPDATE donors set password = '$pw_1',name = '$f_name',dob = '$birthday',sex = '$sex',bloodgroup = '$blood',mobile_no = '$mobile',email = '$email',state = '$state',district = '$district'";
+              $result = mysqli_query($connection,$sql);
+              if($result)
+              {
+                echo "<script type ='text/JavaScript'>";  
+                echo "alert('Registration Successful')";  
+                echo '</script>';
+              }
+              else
+              {
+                die(mysqli_error($connection));
+              }
+          }
+        }
+      }
+    }
+  }  
+  else
+  {
+    echo "<script type ='text/JavaScript'>";  
+    echo "alert('Registration Failed')";  
+    echo '</script>';
+  }
+  echo "<script type='text/javascript'>";
+  echo 'window.location.href = "http://localhost:8080/blood_donation/admin/display.php" ';
+  echo '</script>';
+?>
