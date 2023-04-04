dc88d3accd30c6b09bd797ca0fe107586b0e909a
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-04 11:49:03 +0530

    .

--- admin/add_user.php
+++ admin/add_user.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     require 'C:\xampp\htdocs\blood_donation\db_connection.php';
     session_start();
@@ -79,3 +88,6 @@
   }
   header('Location:C:\xampp\htdocs\blood_donation\admin\display.php');
 ?>
+    
+</body>
+</html>
