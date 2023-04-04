36e418559aaf9f27b9ff0eb96cb10fc325ee7f72
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-04 15:26:09 +0530

    m

--- admin/update.php
+++ admin/update.php
@@ -1,11 +1,11 @@
 <?php
   require 'C:\xampp\htdocs\blood_donation\db_connection.php';
+  $id = $_GET['update_id'];
   if(isset($_POST['update']))
   {
     if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']))
     {
         $error = 0;
-        $id = $_GET['update_id'];
         $f_name = ucfirst($_POST['name']);
         $birthday = $_POST['dob'];
         $sex = $_POST['sex'];
@@ -43,17 +43,19 @@
             if($result)
             {
                 echo "<script type ='text/JavaScript'>";  
-                echo "alert('Updated Successfully')";  
+                echo "alert('Updated Successfully')";
+                echo 'window.location.href = "http://localhost:8080/blood_donation/admin/display.php" ';
                 echo '</script>';
             }
             else
             {
+                echo "<script type ='text/JavaScript'>";  
+                echo "alert('Not Updated !!!')";
+                echo 'window.location.href = "http://localhost:8080/blood_donation/admin/display.php" ';
+                echo '</script>';
                 die(mysqli_error($connection));
             }
         }
     }
   }
-  echo "<script type='text/javascript'>";
-  echo 'window.location.href = "http://localhost:8080/blood_donation/admin/display.php" ';
-  echo '</script>';
 ?>
