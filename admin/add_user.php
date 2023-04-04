c7caf599b1644cb33900203bab8c6d6f28ecf114
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-04 13:52:18 +0530

    ,

--- admin/add_user.php
+++ admin/add_user.php
@@ -2,21 +2,21 @@
   require 'C:\xampp\htdocs\blood_donation\db_connection.php';
   if(isset($_POST['add']))
   {
-  if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']) && !empty($_POST['password_1']))
-  {
-      $error = 0;
-      $f_name = ucfirst($_POST['name']);
-      $birthday = $_POST['dob'];
-      $sex = $_POST['sex'];
-      $blood = $_POST['blood_type'];
-      $mobile = $_POST['mobile_no'];
-      $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-      $state = $_POST['state_input'];
-      $district = $_POST['district_input'];
-      $pw_1  = $_POST['password_1'];
+    if(!empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['sex']) && !empty($_POST['blood_type']) && !empty($_POST['mobile_no']) && !empty($_POST['email']) &&!empty($_POST['state_input']) && !empty($_POST['district_input']) && !empty($_POST['password_1']))
+    {
+        $error = 0;
+        $f_name = ucfirst($_POST['name']);
+        $birthday = $_POST['dob'];
+        $sex = $_POST['sex'];
+        $blood = $_POST['blood_type'];
+        $mobile = $_POST['mobile_no'];
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $state = $_POST['state_input'];
+        $district = $_POST['district_input'];
+        $pw_1  = $_POST['password_1'];
 
-      function validateAge($birthday, $age = 18)
-      {
+        function validateAge($birthday, $age = 18)
+        {
           // $birthday can be UNIX_TIMESTAMP or just a string-date.
           if(is_string($birthday)) {
               $birthday = strtotime($birthday);
@@ -28,30 +28,30 @@
           }
 
           return true;
-      }
-      $ans = validateAge($birthday,18);
-      if($ans == 0)
-      {
-        $error = $error + 1;
-      }
-
-      if($error == 0)
-      {
-        $query = "Select * from donors where email = '$email' || mobile_no = '$mobile' ";
-        $res = mysqli_query($connection,$query);
-        if($res)
+        }
+        $ans = validateAge($birthday,18);
+        if($ans == 0)
         {
-          $num = mysqli_num_rows($res);
-          if($num > 0)
+          $error = $error + 1;
+        }
+
+        if($error == 0)
+        {
+          $query = "Select * from donors where email = '$email' || mobile_no = '$mobile' ";
+          $res = mysqli_query($connection,$query);
+          if($res)
           {
-            echo '<script>alert("USER ALREADY EXISTS")</script>';
-            header('location: C:\xampp\htdocs\blood_donation\admin\display.php');
-          }
-          else
-          {
-            $sql = "INSERT INTO donors (password,name,dob,sex,bloodgroup,mobile_no,email,state,district)VALUES ('$pw_1','$f_name','$birthday','$sex','$blood','$mobile','$email','$state','$district')";
-            $result = mysqli_query($connection,$sql);
-            if($result)
+            $num = mysqli_num_rows($res);
+            if($num > 0)
+            {
+              echo '<script>alert("USER ALREADY EXISTS")</script>';
+              header('location: C:\xampp\htdocs\blood_donation\admin\display.php');
+            }
+            else
+            {
+              $sql = "INSERT INTO donors (password,name,dob,sex,bloodgroup,mobile_no,email,state,district)VALUES ('$pw_1','$f_name','$birthday','$sex','$blood','$mobile','$email','$state','$district')";
+              $result = mysqli_query($connection,$sql);
+              if($result)
             {
               echo '<script>alert("Registration Successful !! ")</script>';
             }
@@ -62,15 +62,15 @@
           }
         }
       }
-     }
+    }
+  }  
   else
   {
     echo "<script type ='text/JavaScript'>";  
     echo "alert('Registration Failed')";  
     echo '</script>';
-    }
   }
-  <script type="text/javascript">
-      window.location.href = 'http://localhost:8080/blood_donation/admin/display.php';
-  </script>
+  echo "<script type='text/javascript'>";
+  echo 'window.location.href = "http://localhost:8080/blood_donation/admin/display.php" ';
+  echo '</script>';
 ?>
