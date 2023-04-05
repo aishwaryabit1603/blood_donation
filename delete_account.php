c2c2a41ce8ab03c5c54360c4723b01ade95fbc39
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-05 16:56:52 +0530

    .

--- delete_account.php
+++ delete_account.php
@@ -1,25 +1,24 @@
 <?php
     require 'C:\xampp\htdocs\blood_donation\db_connection.php';
     session_start();
-    if($_SESSION['email'] != true)
+    if(!isset($_SESSION['email']))
     {
         echo "<script>alert('login first !!')</script>";
         header('Location: login.html');
     }
-
+    
     $id = $_SESSION['email'];
-    $sql = "delete from donors where id_no = $id";
-    $result = mysqli_query($connection,$sql);
-    if($result)
+    $sql = "DELETE FROM donors WHERE id_no = ?";
+    $stmt = mysqli_prepare($connection, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    if(mysqli_stmt_execute($stmt))
     {
         echo "<script>alert('Deleted Successfully !!')</script>";
-
-        $URL="http://localhost:8080/blood_donation/index.html";
-        echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
-        echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+        header('Location: http://localhost:8080/blood_donation/index.html');
     }
     else
     {
         die(mysqli_error($connection));
     }
+    
 ?>
