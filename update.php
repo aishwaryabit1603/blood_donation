1a730537d6048c9c5c33fa42460dbb370d2a49e6
Aishwarya <aishwaryasingla123@gmail.com>
2023-04-05 16:58:57 +0530

    ,

--- update.php
+++ update.php
@@ -1,5 +1,17 @@
 <?php
-  require 'C:\xampp\htdocs\blood_donation\db_connection.php';
+  $db_host = 'localhost';
+  $db_user = 'root';
+  $db_password = "";
+  $db_database = "blood_donation";
+
+  // connecting to database
+  $connection = mysqli_connect($db_host,$db_user,$db_password,'blood_donation');
+  // if connection fails alert message box
+  if(!$connection)
+  {
+      die('Connect not Connect: '.mysqli_error($connection));
+  }
+
   session_start();
   $id = $_SESSION['email']; 
 
